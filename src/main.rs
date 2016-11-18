9527d54500a8f5b91cd9041905f6e8021bdf911a
Dale Wijnand <dale.wijnand@gmail.com>
2016-11-18 01:17:03 +0000

    Print the CWD

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // We assume that we are in a valid directory.
+    let p = env::current_dir().unwrap();
+    println!("The current directory is {}", p.display());
 }
