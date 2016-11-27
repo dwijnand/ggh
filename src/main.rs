07970dc12df42caab37fa737b5e9716b4db50e0e
Dale Wijnand <dale.wijnand@gmail.com>
2016-11-27 18:19:39 +0000

    check if there is a remote "z" branch

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-use std::env;
+extern crate git2;
+
+use git2::*;
+
+fn run() -> Result<(), git2::Error> {
+    let repo: Repository = try!(Repository::open("/d/guava"));
+    let mut remote: Remote = try!(repo.find_remote("dwijnand"));
+    try!(remote.connect(Direction::Fetch));
+    let remote_heads: &[RemoteHead] = try!(remote.list());
+    let mut remote_heads_iter = remote_heads.iter();
+    let z = remote_heads_iter.find(|h| h.name() == "z");
+    match z {
+        Some(..) => println!("found z"),
+        None => println!("not found z"),
+    }
+    Ok(())
+}
 
 fn main() {
-    // We assume that we are in a valid directory.
-    let p = env::current_dir().unwrap();
-    println!("The current directory is {}", p.display());
+    match run() {
+        Ok(()) => {}
+        Err(e) => println!("error: {}", e),
+    }
 }
