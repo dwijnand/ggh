1747cae327cf18456cbf2a7e8bfbb2c4c59ac34c
Dale Wijnand <dale.wijnand@gmail.com>
2017-01-28 15:58:18 +0000

    Extract remote/branch name

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,13 @@ fn remote_callbacks<'a>() -> RemoteCallbacks<'a> {
     let mut cb = RemoteCallbacks::new();
     cb.credentials(|_, _, _| {
         let home = std::env::home_dir().unwrap();
+
         let mut publickey = std::path::PathBuf::from(home.to_owned());
         publickey.push(".ssh/id_rsa.pub");
+
         let mut privatekey = std::path::PathBuf::from(home.to_owned());
         privatekey.push(".ssh/id_rsa");
+
         Cred::ssh_key("git", Some(&publickey), &privatekey, None)
     });
     cb
@@ -32,21 +35,25 @@ fn remote_callbacks<'a>() -> RemoteCallbacks<'a> {
 fn run() -> Result<(), Error> {
     let repo = &try!(Repository::open("/d/guava"));
 
-    let mut remote = try!(repo.find_remote("dwijnand"));
+    let remote_name = "dwijnand";
+    let branch_name = "z";
 
-    println!("Fetching from dwijnand remote");
+    let mut remote = try!(repo.find_remote(remote_name));
+
+    println!("Fetching from {} remote", remote_name);
     try!(remote.fetch(&[], Some(FetchOptions::new().remote_callbacks(remote_callbacks())), None));
 
-    println!("Looking for remote-traching branch z");
-    match repo.find_branch("dwijnand/z", BranchType::Remote) {
-        Ok(..)  => println!("found z"),
-        Err(..) => println!("not found z"),
+    println!("Looking for remote-tracking branch {}", branch_name);
+    match repo.find_branch(&format!("{}/{}", remote_name, branch_name), BranchType::Remote) {
+        Ok(..)  => println!("found {}", branch_name),
+        Err(..) => println!("not found {}", branch_name),
     }
 
-    try!(create_orphan_branch(repo, "z"));
+    try!(create_orphan_branch(repo, branch_name));
 
-    println!("Pushing to dwijnand remote");
-    try!(remote.push(&["+refs/heads/z:refs/heads/z"], Some(PushOptions::new().remote_callbacks(remote_callbacks()))));
+    println!("Pushing to {} remote", remote_name);
+    let refspec = format!("+refs/heads/{}:refs/heads/{}", branch_name, branch_name);
+    try!(remote.push(&[&refspec], Some(PushOptions::new().remote_callbacks(remote_callbacks()))));
 
     println!("Done");
 
@@ -65,15 +72,15 @@ fn create_orphan_branch_force<'repo>(repo: &'repo Repository, name: &str) -> Res
     let tree_b    = try!(repo.treebuilder(None));
     let tree_id   = try!(tree_b.write());
     let tree      = try!(repo.find_tree(tree_id));
-    let sig       = &try!(Signature::new("z", "-", &Time::new(0, 0)));
-    let commit_id = try!(repo.commit(None, sig, sig, "", &tree, &[]));
+    let sig       = try!(Signature::new("z", "-", &Time::new(0, 0)));
+    let commit_id = try!(repo.commit(None, &sig, &sig, "", &tree, &[]));
     let commit    = try!(repo.find_commit(commit_id));
     repo.branch(name, &commit, false)
 }
 
 fn main() {
     match run() {
-        Ok(()) => {}
+        Ok(()) => {},
         Err(e) => error!("{}", e),
     }
 }
